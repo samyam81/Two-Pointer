95b5e27d3bba4c4e48ef213abb913812b354a3ea
samyam81 <samyam081@gmail.com>
2024-06-07 10:22:12 +0545

    SortPArity

--- Two-PointerGO/SortParityONE.go
+++ Two-PointerGO/SortParityONE.go
@@ -1,20 +1,20 @@
 package main
 
 func sortArrayByParity(nums []int) []int {
-	left:=0
-	right:=len(nums)-1
+	left := 0
+	right := len(nums) - 1
 
-	for left<right{
-		if nums[left]%2>nums[right]%2 {
-			temp:=nums[left]
-			nums[left]=nums[right]
-			nums[right]=temp
+	for left < right {
+		if nums[left]%2 > nums[right]%2 {
+			temp := nums[left]
+			nums[left] = nums[right]
+			nums[right] = temp
 		}
-		if nums[left]%2==0 {
+		if nums[left]%2 == 0 {
 			left++
 		}
 
-		if nums[right]%2!=0 {
+		if nums[right]%2 != 0 {
 			right--
 		}
 	}
